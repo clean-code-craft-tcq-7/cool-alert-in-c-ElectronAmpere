cc7a4e8c9b05e92abef706f45f009bc2250c0175
ElectonAmpere <82173777+ElectronAmpere@users.noreply.github.com>
2025-09-30 10:03:20 +0530

    Updated cooling limit coniditon with better handling

--- cooling_alert.c
+++ cooling_alert.c
@@ -3,19 +3,28 @@
 
 // ----------- STEP 2 -----------------
 // Minimal code to compile and fail the test in STEP 2 of test_cooling_alert.cpp
+typedef struct {
+  CoolingType type;
+  const float limit;
+} CoolingLimit_t;
 
-const float PASSIVE_COOLING_LIMIT = 40.0;
-const float HI_ACTIVE_COOLING_LIMIT = 55.0;
+CoolingLimit_t limits[] = {{PASSIVE_COOLING, 40.0}, {HI_ACTIVE_COOLING, 55.0}};
+
+float get_cooling_limit(CoolingType type) {
+  for (int i = 0; i < sizeof(limits) / sizeof(limits[0]); i++) {
+    if (limits[i].type == type)
+      return limits[i].limit;
+  }
+  return 0.0; // or error
+}
 
 Action battery_temperature_to_action(CoolingType cooling_type,
                                      float temperature) {
   Action action;
-  //     // ------- STEP 3 -----------------
-  //     // Minimal code to pass tests in test_cooling_alert.cpp
-  if ((cooling_type == PASSIVE_COOLING &&
-       temperature > PASSIVE_COOLING_LIMIT) ||
-      (cooling_type == HI_ACTIVE_COOLING &&
-       temperature > HI_ACTIVE_COOLING_LIMIT)) {
+
+  float limit = get_cooling_limit(cooling_type);
+
+  if (temperature > limit) {
     action.actionType = ALERT_EMAIL;
     snprintf(action.actionBody, sizeof(action.actionBody),
              "Temperature alert: %.1fF", temperature);
@@ -24,7 +33,6 @@ Action battery_temperature_to_action(CoolingType cooling_type,
     action.actionBody[0] = '\0';
   }
 
-  //     //------- STEP 3 code ends -------
   return action;
 }
 
